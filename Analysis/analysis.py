6831540c300de44965d8ca40befaa6bf73d21aef
JetSpeedFreak <crazygameronline555@gmail.com>
2021-02-08 22:38:39 +0530

    model_without_date_factor

--- Analysis/analysis.py
+++ Analysis/analysis.py
@@ -1,125 +1,72 @@
 #%%
+#Importing the libraries:
 import pandas as pd
 import numpy as np
-np.set_printoptions(precision=4,suppress=True)
 #%%
-#Importing the data:
-train_data=pd.read_csv('D:\ML_Comps\Bike_sharing_demand_pred\Original_data\TRAIN.csv')
-train_len=len(train_data)
-test_data=pd.read_csv('D:\ML_Comps\Bike_sharing_demand_pred\Original_data\TEST.csv')
-data=pd.concat((train_data,test_data),axis=0)
+#Importing the data files:
+train_data = pd.read_csv('D:\ML_Comps\Bike_sharing_demand_pred\Original_data\TRAIN.csv')
+test_data = pd.read_csv('D:\ML_Comps\Bike_sharing_demand_pred\Original_data\TEST.csv')
+combined_data = pd.concat((train_data,test_data),axis=0)
+train_len = len(train_data)
 #%%
-#Checking for null values:
-# for col in data.columns:
-#     extract = data[data[col].isna()]
-#     print(f'{col} : {len(extract)}')
-#No null values in the data
-#%%
-#Date functions:
-def get_day(date):
-    date=date.split('/')
-    return int(date[0])
+#Data prep:
 
-def get_month(date):
-    date=date.split('/')
-    return int(date[1])
+del combined_data['Index'],combined_data['Date']
 
-def get_year(date):
-    date=date.split('/')
-    return int(date[-1])
-#%%
-#Processing the data:
-def process_data(target):
-    #Data cleaning:
-    target['Day']=target['Date'].apply(lambda x:get_day(x))
-    target['Month']=target['Date'].apply(lambda x:get_month(x))
-    # target['Year']=target['Date'].apply(lambda x:get_year(x))
+#encoding the categorical variabes:
+combined_data=pd.get_dummies(combined_data,columns=['Seasons','Holiday','Functioning Day'],drop_first=True)
 
-    del target['Date']
-    del target['Index']
-
-    #encoding the categorical variabes:
-    target=pd.get_dummies(target,columns=['Seasons','Holiday','Functioning Day','Month'],drop_first=True)
-
-    #Binning the dates:
-    bins=[0,5,10,15,20,25,30,35]
-    labels=[0,1,2,3,4,5,6]
-    target['Day_bin']=pd.cut(target['Day'],bins=bins,right=True,labels=labels)
-    del target['Day']
-
-    #encoding the bin values of the target:
-    target=pd.get_dummies(target,columns=['Day_bin'],drop_first=True)
-    return target
-# %%
-# train_data = process_data(train_data)
-# test_data = process_data(test_data)
-data = process_data(data)
 col_order=[ 'Hour', 'Temperature(°C)', 'Humidity(%)', 'Wind speed (m/s)',
        'Visibility (10m)', 'Dew point temperature(°C)',
        'Solar Radiation (MJ/m2)', 'Rainfall(mm)', 'Snowfall (cm)', 'Seasons_Spring', 'Seasons_Summer',
-       'Seasons_Winter', 'Holiday_No Holiday', 'Functioning Day_Yes',
-       'Month_2', 'Month_3', 'Month_4', 'Month_5', 'Month_6', 'Month_7',
-       'Month_8', 'Month_9','Month_10','Month_11', 'Month_12', 'Day_bin_1', 'Day_bin_2', 'Day_bin_3',
-       'Day_bin_4', 'Day_bin_5', 'Day_bin_6','Rented Bike Count']
-# train_data = train_data[col_order]
-# test_data = test_data[col_order]
-data = data[col_order]
+       'Seasons_Winter', 'Holiday_No Holiday', 'Functioning Day_Yes','Rented Bike Count']
+combined_data = combined_data[col_order]
 #%%
-#Preparing the evaluation function:
+#Utitility functions:
 from sklearn.model_selection import cross_val_score
 def evaluate(regressor,ind_vars,dep_vars,name):
     scores=cross_val_score(regressor,X=ind_vars,y=dep_vars,cv=10,n_jobs=-1,scoring='neg_root_mean_squared_error')
-    print(f'{name}:')
+    print(f'\n{name}:')
     print(f'The mean rmse is {scores.mean()}')
     print(f'The std.deviaion is {scores.std()}')
-# %%
-#Linear Regression:
-# Linear Regression:
-# The mean rmse is -445.47761253374193
-# The std.deviaion is 133.19329943718648
+#%%
+#For linear regression:
+# Linear_regression:
+# The mean rmse is -459.98413762136335
+# The std.deviaion is 151.13333078274482
+# del combined_data['Visibility (10m)']
 
-# del data['Month_5'],data['Month_8'],data['Month_9'],data['Month_10'],data['Month_11']
-
-# ind_train=data.iloc[:train_len,:-1].values
-# dep_train=data.iloc[:train_len,-1].values
+# ind_vars = combined_data.iloc[:train_len,:-1].values
+# dep_vars = combined_data.iloc[:train_len,-1].values
 
 # from sklearn.linear_model import LinearRegression
-# regressor=LinearRegression()
+# regressor = LinearRegression()
 
-# evaluate(regressor,ind_train,dep_train,'Linear Regression')
-# %%
+# evaluate(regressor,ind_vars,dep_vars,'Linear_regression')
+#%%
 #Non-Linear Regression:
 # GradBoostReg:
-# The mean rmse is -246.0850438852352
-# The std.deviaion is 94.68234219235138
+# The mean rmse is -245.73063440011484
+# The std.deviaion is 97.29963146417222
 # ExtraTreeReg:
-# The mean rmse is -257.9805599827714
-# The std.deviaion is 106.63336669731588
+# The mean rmse is -249.25570019721226
+# The std.deviaion is 105.16360490413412
 # AdaBoostReg:
-# The mean rmse is -482.84547969358243
-# The std.deviaion is 48.65482533112536
+# The mean rmse is -444.35672495302816
+# The std.deviaion is 67.36587817100026
 # XGBReg:
-# The mean rmse is -267.2840329237818
-# The std.deviaion is 107.65097442040123
+# The mean rmse is -262.7026644688744
+# The std.deviaion is 105.0270663994271
 # XGBRFReg:
-# The mean rmse is -298.86835074663287
-# The std.deviaion is 131.43360636021157
+# The mean rmse is -298.8890634543525
+# The std.deviaion is 131.3796336857591
+
+ind_train = combined_data.iloc[:train_len,:-1].values
+dep_train = combined_data.iloc[:train_len,-1].values
 
 # from sklearn.ensemble import \
 #     RandomForestRegressor,GradientBoostingRegressor,ExtraTreesRegressor,AdaBoostRegressor
 # from xgboost import XGBRegressor,XGBRFRegressor
-
-ind_train=data.iloc[:train_len,:-1].values
-dep_train=data.iloc[:train_len,-1].values
-
-from sklearn.preprocessing import StandardScaler
-scaler=StandardScaler()
-ind_train=scaler.fit_transform(ind_train)
-
-from sklearn.ensemble import GradientBoostingRegressor
-regressor=GradientBoostingRegressor(n_estimators=500)
-regressor.fit(ind_train,dep_train)
-# evaluate(regressor,ind_train,dep_train,f'GradientBoostReg n_est:500, lr:0.1')
 
 # regressors=[
 #     GradientBoostingRegressor(n_estimators=500),
@@ -132,23 +79,31 @@ regressor.fit(ind_train,dep_train)
 
 # for reg,name in zip(regressors,names):
 #     evaluate(reg,ind_train,dep_train,name)
-# %%
-# Model tuning:
-# For first 12 variable(s)(500 est):
-# The mean rmse is -244.48910348356313
-# The std.deviaion is 94.91301107844511
+#%%
+#Model Tuning:
+# n_estimators: 300, lr: 0.1:
+# The mean rmse is -243.12644426633483
+# The std.deviaion is 97.1798465522186
 
 # from sklearn.ensemble import GradientBoostingRegressor
 # est_list=[100,300,500,1000]
-# learning_rates=[1,0.01,0.02,0.001]
-# n_estimators: 1000, lr: 0.02:
-# The mean rmse is -245.1338961848931
-# The std.deviaion is 100.45753981668197
+# learning_rates=[1,0.01,0.1,0.2,0.001]
 
 # for est in est_list:
 #     for lr in learning_rates:
-#         regressor=GradientBoostingRegressor(n_estimators=est,learning_rate=lr)
+#         regressor = GradientBoostingRegressor(n_estimators=est,learning_rate=lr)
+#         ind_train = regressor.transform()
 #         evaluate(regressor,ind_train,dep_train,f'n_estimators: {est}, lr: {lr}')
+#%%
+#Feature Selection:
+
+# For first 9 variable(s):
+# The mean rmse is -242.3579164205192
+# The std.deviaion is 93.84947110549888
+
+from sklearn.ensemble import GradientBoostingRegressor
+regressor = GradientBoostingRegressor(n_estimators=300,random_state=0)
+regressor.fit(ind_train,dep_train)
 
 indices=np.array(list(range(0,len(ind_train[0]))),dtype=int)
 indices=indices.reshape(-1,1)
@@ -157,10 +112,9 @@ scores=scores.reshape(-1,1)
 score_table=np.append(indices,scores,axis=1)
 score_table=score_table[score_table[:,-1].argsort()[::-1]]
 
-target_vars=list(map(int,score_table[:12,0]))
-print(target_vars)
-# %%
-# while len(target_vars)!=0:
-#     regressor=GradientBoostingRegressor(n_estimators=500)
-#     evaluate(regressor,ind_train[:,target_vars],dep_train,f'For first {len(target_vars)} variable(s)')
-#     del target_vars[-1]
+target_vars=list(map(int,score_table[:,0]))
+
+while len(target_vars)!=0:
+    regressor=GradientBoostingRegressor(n_estimators=300,random_state=0)
+    evaluate(regressor,ind_train[:,target_vars],dep_train,f'For first {len(target_vars)} variable(s)')
+    del target_vars[-1]
--- model.py
+++ model.py
@@ -9,67 +9,27 @@ test_data = pd.read_csv('D:\ML_Comps\Bike_sharing_demand_pred\Original_data\TEST
 combined_data = pd.concat((train_data,test_data),axis=0)
 train_len=len(train_data)
 #%%
-#Date functions:
-def get_day(date):
-    date=date.split('/')
-    return int(date[0])
-
-def get_month(date):
-    date=date.split('/')
-    return int(date[1])
-
-def get_year(date):
-    date=date.split('/')
-    return int(date[-1])
-#%%
-#Function for processing the data:
-def process_data(target):
-    #Data cleaning:
-    target['Day']=target['Date'].apply(lambda x:get_day(x))
-    target['Month']=target['Date'].apply(lambda x:get_month(x))
-    
-    del target['Date']
-    del target['Index']
-
-    #encoding the categorical variabes:
-    target=pd.get_dummies(target,columns=['Seasons','Holiday','Functioning Day','Month'],drop_first=True)
-
-    #Binning the dates:
-    bins=[0,5,10,15,20,25,30,35]
-    labels=[0,1,2,3,4,5,6]
-    target['Day_bin']=pd.cut(target['Day'],bins=bins,right=True,labels=labels)
-    del target['Day']
-
-    #encoding the bin values of the target:
-    target=pd.get_dummies(target,columns=['Day_bin'],drop_first=True)
-    return target
-#%%
 #Processing the data:
-combined_data = process_data(combined_data)
-col_order = [ 'Hour', 'Temperature(°C)', 'Humidity(%)', 'Wind speed (m/s)',
+
+del combined_data['Index'],combined_data['Date']
+
+#encoding the categorical variabes:
+combined_data=pd.get_dummies(combined_data,columns=['Seasons','Holiday','Functioning Day'],drop_first=True)
+
+col_order=[ 'Hour', 'Temperature(°C)', 'Humidity(%)', 'Wind speed (m/s)',
        'Visibility (10m)', 'Dew point temperature(°C)',
        'Solar Radiation (MJ/m2)', 'Rainfall(mm)', 'Snowfall (cm)', 'Seasons_Spring', 'Seasons_Summer',
-       'Seasons_Winter', 'Holiday_No Holiday', 'Functioning Day_Yes',
-       'Month_2', 'Month_3', 'Month_4', 'Month_5', 'Month_6', 'Month_7',
-       'Month_8', 'Month_9','Month_10','Month_11', 'Month_12', 'Day_bin_1', 'Day_bin_2', 'Day_bin_3',
-       'Day_bin_4', 'Day_bin_5', 'Day_bin_6','Rented Bike Count']
+       'Seasons_Winter', 'Holiday_No Holiday', 'Functioning Day_Yes','Rented Bike Count']
 combined_data = combined_data[col_order]
 #%%
-#Splitting the data:
-ind_train = combined_data.iloc[:train_len,:-1].values
+#Data segregation:
+ind_vars = combined_data.iloc[:,:-1].values
 dep_train = combined_data.iloc[:train_len,-1].values
-ind_test = combined_data.iloc[train_len:,:-1].values
-#%%
-#Feature Scaling:
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
-ind_train = scaler.fit_transform(ind_train)
-ind_test = scaler.transform(ind_test)
 #%%
 #Filtering the variables(Based on the results obtained from analysis):
-target_vars = [1, 0, 6, 2, 7, 13, 5, 18, 11, 3, 4, 17]
-ind_train = ind_train[:,target_vars]
-ind_test = ind_test[:,target_vars]
+# target_vars = [1, 0, 6, 2, 7, 13, 5, 11, 4]
+ind_train = ind_vars[:train_len,:]
+ind_test = ind_vars[train_len:,:]
 #%%
 #Utilities:
 def refine(x):
@@ -77,7 +37,7 @@ def refine(x):
 #%%
 #Modelling and Prediction:
 from sklearn.ensemble import GradientBoostingRegressor
-regressor = GradientBoostingRegressor(n_estimators=500)
+regressor = GradientBoostingRegressor(n_estimators=300,random_state=0)
 regressor.fit(ind_train,dep_train)
 
 results = regressor.predict(ind_test)
@@ -88,11 +48,5 @@ results = results.reshape(-1,1)
 indices = test_data.iloc[:,0:1].values
 result_table = np.append(indices,results,axis=1)
 result_table = pd.DataFrame(result_table,columns=['Index','Rented Bike Count'])
-result_table.to_csv('D:\ML_Comps\Bike_sharing_demand_pred\submissions\submission_1.csv',index=False)
-# %%
-abnormal_cols=[]
-for i in range(len(results)):
-    if results[i,0] == 0:
-        abnormal_cols.append(i)
-ab_data = test_data.iloc[abnormal_cols,:]
+result_table.to_csv('D:\ML_Comps\Bike_sharing_demand_pred\submissions\submission_4.csv',index=False)
 # %%
